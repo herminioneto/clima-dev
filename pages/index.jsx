a91070b41a269a9c0ee30ede3e37f31b46e9a0c4
Herminio Neto <herminio_neto@outlook.com>
2022-12-23 19:28:15 -0300

    Updated index.js

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 import BgImageWithChildren from '../src/components/layout/bgImageWithChildren/BgImageWithChildren';
 
 const StyledDiv = styled.div`
-  background-color: red;
+  background-color: ${props => props.theme.colors.white};
+  min-width: 500px;
+
+  @media (max-width: 500px) {
+    min-width: 100%;
+  }
 `;
 
 export default function HomePage() {
