e299012f2ac74ebbf246b9062be53b9489d978f7
Herminio Neto <herminio_neto@outlook.com>
2022-12-23 19:22:29 -0300

    Created bg img with children

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
 
+import BgImageWithChildren from '../src/components/layout/bgImageWithChildren/BgImageWithChildren';
+
 const StyledDiv = styled.div`
   background-color: red;
 `;
 
 export default function HomePage() {
   return (
-    <StyledDiv>
-      HomePage
-    </StyledDiv>
+    <BgImageWithChildren>
+      <StyledDiv>
+        HomePage
+      </StyledDiv>
+    </BgImageWithChildren>
   );
 };
---
+++ src/components/layout/bgImageWithChildren/BgImageWithChildren.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+const StyledFlex = styled.div`
+  display: flex;
+`;
+
+const StyledImage = styled.div`
+  background-image: url('${props => props.image}');
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: cover;
+
+  width: 100%;
+  min-height: 100vh;
+`;
+
+function BgImageWithChildren({ children, image }) {
+  return (
+    <StyledFlex>
+      {children}
+      <StyledImage image={image}/>
+    </StyledFlex>
+  );
+};
+
+BgImageWithChildren.defaultProps = {
+  image: '/background.png'
+};
+
+export default BgImageWithChildren
